45fbb6d29b3562dd19b2459836d9484180b5baa7
Imam_said <saidouhw02@gmail.com>
2024-06-05 16:32:24 +0000

    add the others functions

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-def calcul(a,b):
-    return a+b
+def calcul(a, b, operation):
+    if operation == 'add':
+        return a + b
+    elif operation == 'subtract':
+        return a - b
+    elif operation == 'multiply':
+        return a * b
+    elif operation == 'divide':
+        if b != 0:
+            return a / b
+        else:
+            return "Erreur : division par zéro"
+    else:
+        return "Erreur : opération non valide"
